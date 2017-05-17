3901561835688cd8ae35d41468b49afa05653ada
rizaldi <rizaldichozzone@gmail.com>
2017-05-17 10:52:35 +0700

    update field

--- database/migrations/2017_05_17_033814_create_tours_table.php
+++ database/migrations/2017_05_17_033814_create_tours_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCountriesTable extends Migration
+class CreateToursTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateCountriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('countries', function (Blueprint $table) {
+        Schema::create('tours', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('code');
+            $table->text('image');
             $table->string('name');
+            $table->double('latitude');
+            $table->double('longitude');
             $table->text('description');
-            $table->text('flag');
+            $table->string('location');
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateCountriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('countries');
+        Schema::dropIfExists('tours');
     }
 }
