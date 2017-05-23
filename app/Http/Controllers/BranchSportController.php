d1b74fb1226e1ec591412a12ba3fab54fec9d4b4
rizaldi <rizaldichozzone@gmail.com>
2017-05-23 09:28:29 +0700

    remove token laravel , add nearby tour and add nearby competition

--- app/Http/Controllers/BranchSportController.php
+++ app/Http/Controllers/BranchSportController.php
@@ -12,6 +12,8 @@ class BranchSportController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct(){
+    }
     public function index()
     {
         return response()->json(BranchSport::get());
@@ -81,5 +83,28 @@ class BranchSportController extends Controller
     public function destroy($id)
     {
         //
+    }
+    public function nearby_competition()
+    {
+
+        $inputJSON = file_get_contents('php://input');
+        $decode = json_decode($inputJSON, true);
+        
+        $get_lat = $decode['data'][0]['latitude'];
+        $get_long = $decode['data'][0]['longitude'];
+
+        $branch_sport = BranchSport::get();
+        foreach ($branch_sport as $branch) {
+            $datamaps = file_get_contents("https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=".$get_lat.",".$get_long."&destinations=".$branch['latitude'].",".$branch['longitude']."&key=%20AIzaSyCWpwVwu1hO6TJW1H8x_zlhrLfbSbQ2r3o");
+            $decode_maps = json_decode($datamaps,true);
+            $distance = $decode_maps['rows'][0]['elements'][0]['distance']['value'];
+            // echo $distance."<br>";
+            if ($distance < 1500000) {
+                $data[] = $branch;
+            }else{
+                $data[] = "lewat";
+            }
+            return response()->json($data);
+        }
     }
 }
